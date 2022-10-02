63cf092d8441f914bef4c612fdeb79c89d46f1e9
Sia McQuillan <mcqui064@d.umn.edu>
2022-10-02 13:23:05 -0500

    wknd2

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,1 +1,23 @@
 # Start Your Code here
+class Parking_Garage():
+    def __init__(Self, Take_Ticket, Pay_for_parking, Variable, Leave_Garage):
+        self.Take_Ticket = Take_Ticket
+        self.Pay_for_Parking = Pay_for_Parking
+        self.Variable = Variable
+        self.Leave_Garage = Leave_Garage
+
+def Take_Ticket():
+    pass
+
+def Pay_for_Parking():
+    pass
+
+def Variable():
+    pass
+
+def Leave_Garage():
+    pass
+
+#-----------------------------------------------------------------------------------
+#--------------------------------MAIN PART OF CODE----------------------------------
+Welcome = input("Welcome to DS's Parking Garage! If you would like to park please take a ticket.")
