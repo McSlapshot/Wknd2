67a3c9e6735d988dd5d3fbe6b23932ce5644a586
Sia McQuillan <mcqui064@d.umn.edu>
2022-10-02 18:57:50 -0500

    final addition

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,57 +1,56 @@
 # Start Your Code here
 
+from re import T
+
+
 class Parking_Garage():
-    def __init__(self, spots_available, Pay_for_Parking, add_car, remove_car):
-        self.spots_available = spots_available
-        self.Pay_for_Parking = Pay_for_Parking
-        self.add_car = add_car
-        self.spots = 9
-        self.tickets = 9
-        self.remove_car = remove_car
 
-def spots_available(self):       
-        return self.spots
+    def add_car(tickets, spots):
+        if len(tickets) == 0:
+            print("We don't have any space available")
+        else:
+            print("Here is your ticket " + str(tickets.pop()) + " and your parking spot is " + str(spots.pop()))
 
-def Pay_for_Parking(self):
-    Price = input("Please input $5 to pay your ticket")
-    if Price == '$5':
-        print("Thank you! The ticket has been paid, you have 15 min to leave, come again!") 
-    else:
-        print("Incorrect payment, try again")    
 
-def add_car(self):
-    print("Please take a ticket")
-    if self.spots > 0:
-        self.spots -=1
-        self.tickets -= 1
+    def Pay_for_Parking(tickets, spots):
+        Price = input("Please input $5 to pay your ticket")
+        if Price == '$5':
+            Parking_Garage.remove_car(tickets, spots)
+            print("Thank you! The ticket has been paid, you have 15 min to leave, come again!") 
+        else:
+            print("Incorrect payment, try again")
+            return    
 
-    else:
-        print("We don't have any space available")
 
-def remove_car(self):
-    self.spots += 1
-    self.tickets +=1
+    def remove_car(tickets, spots):
+        paid = input('Do you want to pay your ticket? y/n')
+        if len(tickets) == 9:
+            print("ticket paid for, have a great day!")
+        elif paid.lower() == 'y':
+            tickets.append(tickets[-1]+1)
+            spots.append(spots[-1]+1)
+            print("You have paid your ticket, have a great day!")
+        else:
+            paid.lower() == 'n'
+            print("Please come back when you wish to pay your ticket")
+            
 
- 
 
 #-----------------------------------------------------------------------------------
 #--------------------------------MAIN PART OF CODE----------------------------------
-Current_Ticket = {}
 
-DS_Garage = Parking_Garage(spots_available, Pay_for_Parking, add_car, remove_car)
+spots = [1,2,3,4,5,6,7,8,9]
+tickets = [1,2,3,4,5,6,7,8,9]
+
 
 def Garage():
-
     while True:
-    
-        Welcome = input("Welcome to DS's Parking Garage! If you would like to park please say yes to take a ticket.")
-
-        if Welcome == "yes":
-            DS_Garage.add_car()            
-
-        Leaving = input('Enter done when leaving')
-        if Leaving == 'done':
-            Pay_for_Parking()
-            remove_car()     
-
+        Welcome = input("Welcome to DS's Parking Garage! If you would like to 't' take ticket, 'p' pay for parking or 'l' to leave.")
+        if Welcome == "t":
+            Parking_Garage.add_car(tickets, spots)            
+        elif Welcome == 'p':
+            Parking_Garage.Pay_for_Parking(tickets, spots)
+        elif Welcome == "l":
+            Parking_Garage.remove_car(tickets, spots)
+            break
 Garage()
--- pract.py
+++ pract.py
@@ -2,3 +2,9 @@ Tickets = [1,2,3,4,5]
 taken_ticket = Tickets.pop(-1)
 print('your ticket #: ' + str(taken_ticket))
 print('tickets left: ' + str(Tickets))
+
+def __init__(self, spots_available, Pay_for_Parking, add_car, remove_car):
+        self.spots_available = spots_available
+        self.Pay_for_Parking = Pay_for_Parking
+        self.add_car = add_car
+        self.remove_car = remove_car
